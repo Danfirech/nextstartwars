d0f39bd94e627782c093f237a677b0d9c503d76d
Dan Possehl <danielbpossehl@gmail.com>
2021-12-19 12:42:04 -0700

    created people page and connected API data

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,36 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      <h1 className="text-2xl text-center my-6 font-semibold">
+        Welcome to Next.js Star Wars Website
+      </h1>
+      <br />
+      <p>
+        Please LIKE / SHARE to keep our generators available! Click Like The
+        chair sat in the corner where it had been for over 25 years. The only
+        difference was there was someone actually sitting in it. How long had it
+        been since someone had done that? Ten years or more he imagined. Yet
+        there was no denying the presence in the chair now.{" "}
+      </p>
+      <br />
+      <p>
+        Please LIKE / SHARE to keep our generators available! Click Like The
+        chair sat in the corner where it had been for over 25 years. The only
+        difference was there was someone actually sitting in it. How long had it
+        been since someone had done that? Ten years or more he imagined. Yet
+        there was no denying the presence in the chair now.{" "}
+      </p>
+      <br />
+      <p>
+        Please LIKE / SHARE to keep our generators available! Click Like The
+        chair sat in the corner where it had been for over 25 years. The only
+        difference was there was someone actually sitting in it. How long had it
+        been since someone had done that? Ten years or more he imagined. Yet
+        there was no denying the presence in the chair now.{" "}
+      </p>
+      <br />
     </div>
   );
 }
