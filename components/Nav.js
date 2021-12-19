cce28a5cd1b18e474c6acb809583f64f8715d89a
Dan Possehl <danielbpossehl@gmail.com>
2021-12-19 12:04:50 -0700

    nav finished

--- components/Nav.js
+++ components/Nav.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function Nav() {
   return (
     <div className="py-4 px-8 bg-gray-700 text-white flex justify-between">
-      <div>Starwars API</div>
-      <div>
-        <div>People</div>
-        <div>Planets</div>
-        <div>Vehicles</div>
+      <div className="text-xl">Starwars API</div>
+      <div className="flex items-center">
+        <div className="ml-8">People</div>
+        <div className="ml-8">Planets</div>
+        <div className="ml-8">Vehicles</div>
       </div>
     </div>
   );
