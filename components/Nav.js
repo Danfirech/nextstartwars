d50ab455d93175725d7cb5819b2a982af10c399c
Dan Possehl <danielbpossehl@gmail.com>
2021-12-19 11:46:43 -0700

    created framework for starwars site

---
+++ components/Nav.js
@@ -0,0 +1,15 @@
+import react from "react";
+import Nav from "./Nav";
+
+export default function Nav() {
+  return (
+    <div className="py-4 px-8 bg-gray-700 text-white flex justify-between">
+      <div>Starwars API</div>
+      <div>
+        <div>People</div>
+        <div>Planets</div>
+        <div>Vehicles</div>
+      </div>
+    </div>
+  );
+}
