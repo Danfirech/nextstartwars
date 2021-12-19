23280423c358d6ec696f82472e6fdcdbe8e32818
Dan Possehl <danielbpossehl@gmail.com>
2021-12-19 12:08:56 -0700

    added Links

--- components/Nav.js
+++ components/Nav.js
@@ -1,13 +1,22 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Nav() {
   return (
     <div className="py-4 px-8 bg-gray-700 text-white flex justify-between">
-      <div className="text-xl">Starwars API</div>
+      <div className="text-xl">
+        <Link href="/">Starwars API</Link>
+      </div>
       <div className="flex items-center">
-        <div className="ml-8">People</div>
-        <div className="ml-8">Planets</div>
-        <div className="ml-8">Vehicles</div>
+        <div className="ml-8">
+          <Link href="/people">People </Link>
+        </div>
+        <div className="ml-8">
+          <Link href="/planets">Planets</Link>
+        </div>
+        <div className="ml-8">
+          <Link href="/vehicles">Vehicles</Link>
+        </div>
       </div>
     </div>
   );
